85ab6c116120326ac2de54d877dc81e81a19a62f
mj bayat <you@example.com>
2022-11-02 18:58:58 +0330

    product finished

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -11,23 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEBUG = True
+IS_DEVEL = True
+
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zoj=@s72t*e92xr=x&rf1$1owy8n$@wmo$iqrga35ijl6#fup7'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -70,8 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'digital_products.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -80,8 +76,6 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -98,27 +92,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.utils.translation import ugettext_lazy as _
 
 class Category(models.Model):
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
-    title = models.CharField(_('title'), max_length=50)
+    title = models.CharField(_('title'), max_length=50 )
     description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True , upload_to='categories/')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
@@ -15,6 +15,9 @@ class Category(models.Model):
         db_table = 'categories'
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.title
 
 
 class Product(models.Model):
@@ -30,6 +33,8 @@ class Product(models.Model):
         db_table = 'products'
         verbose_name = _('product')
         verbose_name_plural = _('products')
+    def __str__(self):
+        return self.title
 
 
 class File(models.Model):
@@ -54,3 +59,6 @@ class File(models.Model):
         db_table = 'files'
         verbose_name = _('file')
         verbose_name_plural = _('files')
+
+    def __str__(self):
+        return self.title
